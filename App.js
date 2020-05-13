97d1b7d4eade0631a0350499f49c5f8e57939037
Oskari Holopainen <oskariholopa@gmail.com>
2020-05-13 21:42:44 +0300

    Render reservations in horizontal and vertical scrollable grid

--- App.js
+++ App.js
@@ -1,59 +1,94 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Button, FlatList, Alert } from 'react-native'
+import { StyleSheet, Text, View, Button, Alert, ScrollView, Dimensions, TouchableOpacity } from 'react-native'
+const DimensionsWindowWidth = Dimensions.get("window").width;
+import { Table, Row, Rows, Col, Cols, TableWrapper } from 'react-native-table-component';
 
 export default function App() {
   const [events, setEvents] = useState([])
 
+  const tableHead = ['Court 1', 'Court 2', 'Court 3', 'Court 4', 'Court 5', 'Court 6']
+  const widthArr = [160, 160, 160, 160, 160, 160]
+
   useEffect(() => {
     getEvents('tennis')
+    console.log(DimensionsWindowWidth)
   }, [])
-  
-const getEvents = type => {
-  const baseUrl = 'http://dev.api.northly.fi/reservations'
-  const today = new Date().toISOString().split('T')[0]
-  const url = `${baseUrl}?date=${today}&type=${type}`
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    setEvents(data)
-  })
-  .catch((error) => {
-    Alert.alert('Error', error);
-  })
-}
-  
-  const handleReserveButtonClick = event => {
-    console.log('Reserve event:', event)
+
+  const getEvents = type => {
+    const baseUrl = 'http://dev.api.northly.fi/reservations'
+    const today = new Date().toISOString().split('T')[0]
+    const url = `${baseUrl}?date=${today}&type=${type}`
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data)
+      })
+      .catch((error) => {
+        Alert.alert('Error', error);
+      })
   }
+
+  const handleReserveButtonClick = event => {
+    console.log('Reserve event:', event.id)
+  }
+
+  const renderButton = (event) => (
+    <View style={styles.cell}>
+      <Text style={styles.text}>Court ID: {event.courtId}</Text>
+      <Text style={styles.text}>Start: {event.start}</Text>
+      <Text style={styles.text}>End: {event.end}</Text>
+      <TouchableOpacity onPress={() => handleReserveButtonClick(event)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>Reserve</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+
+  const tableData = [];
+
+  for (let i = 1; i <= 6; i++) {
+    let columndata = []
+    events.forEach(event => {
+      if (Number(event.courtId) === i) {
+        columndata.push(renderButton(event))
+      }
+    })
+    tableData.push(columndata)
+  }
+
 
   return (
     <View style={styles.container}>
-      <Text>Tennispallo</Text>
-      {events.length === 0 ? null : (
-        <FlatList
-        style={{ marginTop: 50, flex: 1, backgroundColor: '#fafafa', width: 395}}
-        data={events}
-        renderItem={({ item }) => (
-          <View style={{margin: 10}}>
-            <Text>ID: {item.id}</Text>
-            <Text>Type: {item.type}</Text>
-            <Text>Court: {item.courtId}</Text>
-            <Text>Start: {item.start}</Text>
-            <Text>End: {item.end}</Text>
-            <Button title={'varaa'} onPress={() => handleReserveButtonClick(item)} />
-          </View>
-        )}
-      />
-      )}
+      <ScrollView horizontal={true}>
+        <View>
+          <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+            <Row data={tableHead} widthArr={widthArr} style={styles.header} textStyle={styles.text} />
+          </Table>
+          <ScrollView style={styles.dataWrapper}>
+            <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+              <TableWrapper style={{ flex: 1 }}>
+                <Cols data={tableData} widthArr={widthArr} heightArr={[100, 100, 100, 100, 100, 100, 100, 100]} textStyle={styles.text} />
+              </TableWrapper>
+            </Table>
+          </ScrollView>
+        </View>
+      </ScrollView>
     </View>
   )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  header: { height: 50, backgroundColor: '#10a300' },
+  text: { textAlign: 'center', fontSize: 8 },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' },
+  btn: { width: 58, height: 18, marginLeft: 15, backgroundColor: '#c8e1ff', borderRadius: 2 },
+  btnText: { textAlign: 'center' },
+  cell: {
+    display: 'flex',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
+
